fdeffb68686f6bea7146577f52b7b2f0b4592c0b
Jerzy Andrzejewski <230347@edu.p.lodz.pl>
2021-08-10 20:22:40 +0200

    cores and memory

--- cpuandmem.java
+++ cpuandmem.java
@@ -8,25 +8,6 @@ class Cpuandmem
     public static void main(String args[])
 	{
         System.out.println(Runtime.getRuntime().availableProcessors());
-		Cpuandmem cpuandmem = new Cpuandmem();
-		cpuandmem.cpu();
-        cpuandmem.memory();
-	}
-    
-    public void memory()
-	{
-		/*System.out.println(String.format("Initial memory: %.2f GB", 
-          (double)memoryMXBean.getHeapMemoryUsage().getInit() /1073741824));
-        System.out.println(String.format("Used heap memory: %.2f GB", 
-          (double)memoryMXBean.getHeapMemoryUsage().getUsed() /1073741824));
-        System.out.println(String.format("Max heap memory: %.2f GB", 
-          (double)memoryMXBean.getHeapMemoryUsage().getMax() /1073741824));
-        System.out.println(String.format("Committed memory: %.2f GB", 
-          (double)memoryMXBean.getHeapMemoryUsage().getCommitted() /1073741824));*/
-	}
-    
-    public void cpu()
-	{
-		//threadMXBean.getThreadCount();
+        System.out.println(Runtime.getRuntime().totalMemory());
 	}
 }
