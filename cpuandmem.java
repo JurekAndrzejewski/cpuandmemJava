9de99c0e5327d762009659764e827dd3b50b886c
Jerzy Andrzejewski <230347@edu.p.lodz.pl>
2021-08-10 20:30:56 +0200

    GBs and GiBs included

--- cpuandmem.java
+++ cpuandmem.java
@@ -1,13 +1,17 @@
 import java.lang.management.ManagementFactory;
-
+static int KB = 1000
+static int MB = 1000*KB
+static int GB = 1000*MB
+static int KiB = 1024
+static int MiB = 1024*KiB
+static int GiB = 1024*MiB
 class Cpuandmem
 {
-    //MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
-    //ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
-	
     public static void main(String args[])
 	{
-        System.out.println(Runtime.getRuntime().availableProcessors());
-        System.out.println(Runtime.getRuntime().totalMemory());
+        System.out.println("Number of processors available to the Java virtual machine: ");
+        System.out.println(Runtime.getRuntime().availableProcessors()+"\n");
+        System.out.println("Total amount of memory currently available: ");
+        System.out.println((Runtime.getRuntime().totalMemory())/GB+" GB or "(Runtime.getRuntime().totalMemory())/GiB+" GiB");
 	}
 }
